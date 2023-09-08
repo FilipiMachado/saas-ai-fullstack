e60f89a4a06a375a9d9873182e3a7de7e39147d7
filipi <filipisee@hotmail.com>
2023-09-08 20:24:43 -0300

    working on dashboard page

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,3 +1,19 @@
+"use client";
+
+import { MessageSquare } from "lucide-react";
+
+import { Card } from "@/components/ui/card";
+
+const tools = [
+  {
+    label: "Conversation",
+    icon: MessageSquare,
+    color: "text-violet-500",
+    bgColor: "bg-violet-500/10",
+    href: "/conversation",
+  },
+];
+
 export default function DashboardPage() {
   return (
     <div>
@@ -5,10 +21,18 @@ export default function DashboardPage() {
         <h2 className="text-2xl md:text-4xl font-bold text-center">
           Explore the possibilities of what AI can deliver!
         </h2>
-        <p>
+        <p className="text-muted-foreground font-light text-sm md:text-lg text-center">
           Engage on a conversation with the latest AI technology and experience
           the wide-ranging capabilities it provides
         </p>
+      </div>
+      <div className="px-4 md:px-20 lg:px-32 space-y-4">
+        {tools.map((tool) => (
+          <Card
+            key={tool.href}
+            className="p-4 border-black/5 flex items-center justify-between hover:shadow-md transition cursor-pointer"
+          ></Card>
+        ))}
       </div>
     </div>
   );
