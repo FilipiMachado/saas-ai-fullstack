0841f6913fac6e8e198061837fe94b17929e0e7d
filipi <filipisee@hotmail.com>
2023-09-08 19:47:37 -0300

    finished mobile sidebar

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,7 +1,15 @@
 export default function DashboardPage() {
   return (
     <div>
-      Dashboard Page (Protected)
+      <div className="mb-8 space-y-4">
+        <h2 className="text-2xl md:text-4xl font-bold text-center">
+          Explore the possibilities of what AI can deliver!
+        </h2>
+        <p>
+          Engage on a conversation with the latest AI technology and experience
+          the wide-ranging capabilities it provides
+        </p>
+      </div>
     </div>
-  )
+  );
 }
--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -3,12 +3,32 @@
 import { Menu } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import Sidebar from "@/components/sidebar";
+import { useEffect, useState } from "react";
 
 const MobileSidebar = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
   return (
-    <Button className="md:hidden" variant="ghost" size="icon">
-      <Menu />
-    </Button>
+    <Sheet>
+      <SheetTrigger>
+        <Button className="md:hidden" variant="ghost" size="icon">
+          <Menu />
+        </Button>
+      </SheetTrigger>
+      <SheetContent side="left" className="p-0">
+        <Sidebar />
+      </SheetContent>
+    </Sheet>
   );
 };
 
